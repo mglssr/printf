c94cd46f9d41bc8a73d97cdb0dfc59a906e5fe10
mglssr <iglesiasmaia6@gmail.com>
2022-03-12 06:18:47 -0800

    solo declaré 3 variables jeje

---
+++ printf.c
@@ -0,0 +1,16 @@
+#include "printf_main.h"
+#include <stdarg.h>
+
+/**
+* _printf - da un output acorde a un formato pasado (pend cambio)
+* @format: una string (pend cambio)
+* Return: retorna el numero de chars printeados
+* rf: variable que Recorre Format
+* rars: variable que Recorre ARray usando Struct
+*/
+
+int _printf(const char *format, ...);
+{
+	va_list args;
+	int rf, rars;
+}
