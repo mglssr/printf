46ef29a40321cbceed7ad79d53d3ebada0db384c
githubjoaco16 <4379@holbertonschool.com>
2022-03-12 15:10:24 +0000

    finalizando funcion principal, incluido strlen 'casero'

---
+++ _strlen.c
@@ -0,0 +1,12 @@
+#include <string.h>
+#include "main.h"
+/**
+* _strlen  -  returns the length of a string
+* @s: string
+* Return: len
+*/
+int _strlen(char *s)
+{
+	size_t strlen(const char *s);
+	return (strlen(s));
+}
